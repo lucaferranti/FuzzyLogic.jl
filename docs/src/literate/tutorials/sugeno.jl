ae2cea05531df729bd8eaae619fdb058c2fbe282
lucaferranti <lucaa.ferranti@gmail.com>
2023-01-22 11:30:29 +0200

    fix typos

--- docs/src/literate/tutorials/sugeno.jl
+++ docs/src/literate/tutorials/sugeno.jl
@@ -2,12 +2,12 @@
 # Build a Sugeno inference system
 
 This tutorial describes how to construct a type-1 Sugeno inference system.
-The reader is assumed to be familiar with the basic syntax to build an inference syntax, which
+The reader is assumed to be familiar with the basic syntax to build a fuzzy system, which
 is described in the [Build a Mamdani inference system](@ref) tutorial.
 
 DOWNLOAD_NOTE
 
-Similarly to Mamdani, a Sugeno inference system can be built using the [`@sugfis`](@ref) macro.
+A Sugeno inference system can be built using the [`@sugfis`](@ref) macro.
 The following example shows the macro in action
 
 =#
@@ -40,16 +40,16 @@ fis = @sugfis function tipper(service, food)::tip
 end
 
 #=
-The results is an object of type [`SugenoFuzzySystem`](@ref). This similar to a Mamdani inference system, with the main difference being in
+The result is an object of type [`SugenoFuzzySystem`](@ref). This is similar to a Mamdani, with the main difference being in
 the output definition.
-In a Sugeno system, the output "membership function" can be:
+In a Sugeno system, the output "membership functions" can be:
 
-- a constant output, e.g. `average = 15`. This means that if the tip is average, then it has value ``15``,
-- an affine combination of the inputs, e.g. ``generous = 2service, 0.5food, 5.0``. This means that if the tip is generous, then it will have output value ``2service+0.5food + 5.0``.
+- A [`ConstantSugenoOutput`](@ref), e.g. `average = 15`. This means that if the tip is average, then it has constant value ``15``,
+- A [`LinearSugenoOutput`](@ref), e.g. `generous = 2service, 0.5food, 5.0`. This means that if the tip is generous, then its value will be ``2service+0.5food + 5.0``.
 
-It is good to highlight that this functions return the value of the output variable and not a membership degree, like in a Mamdani system.
+It is good to highlight that these functions return the value of the output variable and not a membership degree, like in a Mamdani system.
 
-The second difference from a Mandani system is the settings that cab be tuned. A Sugeno system has only the following options:
+The second difference from a Mandani system is in the settings that can be tuned. A Sugeno system only has the following options:
 
 - `and`: algorithm to evaluate `&&`. Must be one of the available [Conjuction methods](@ref). Default [`ProdAnd`](@ref).
 - `or`: algorithm to evaluate `||`. Must be one of the available [Disjunction methods](@ref). Default [`ProbSumOr`](@ref)
@@ -60,14 +60,14 @@ The created model can be evaluated the same way of a Mamdani system.
 
 fis(service = 2, food = 3)
 
-# Let's see how to plot the output variables, as it differs from the Mamdani system
+# Let's see the plotting of output variables, as it differs from the Mamdani system
 
 plot(fis, :tip)
 
 #=
 As you can see
 
-- If the membership function is constant, then the plot simply shows a horizontal line at the output value level
+- If the membership function is constant, then the plot simply shows a horizontal line at the output value level.
 - For `LinearSugenoOutput`, the plot is a bar plot, showing for each input variable the corresponding coefficient.
   This gives a visual indication of how much each input contributes to the output.
 

03fd58e9c81896a070ad1ee5d9702557f0d6a051
lucaferranti <lucaa.ferranti@gmail.com>
2023-09-16 17:47:33 +0300

    upgrade to Documenter 1.0

--- docs/setup.jl
+++ docs/setup.jl
@@ -3,7 +3,3 @@ import Pkg
 Pkg.activate(@__DIR__)
 Pkg.develop(Pkg.PackageSpec(path = joinpath(@__DIR__, "..")))
 Pkg.instantiate()
-
-# TODO remove once Documenter 0.28 is released
-# this is to get both the edit link and a the github link
-Pkg.add(Pkg.PackageSpec(name = "Documenter", rev = "30baed5"))
